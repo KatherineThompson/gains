6c5da12f5876c5359f5f9f4cb7c73fb00609db75
Nick Heiner <nick.heiner@opower.com>
2016-02-05 14:45:35 -0800

    Make data stuff work.

--- jquery/README.md
+++ jquery/README.md
@@ -4,3 +4,20 @@
 * http://learn.jquery.com/
 * http://api.jquery.com/
 * http://learn.jquery.com/about-jquery/additional-support/
+
+## Setup
+This project was developed with Node 5.x, so if you have that
+on your machine, it will work for you.
+
+## Instructions
+To install the project's dependencies, run:
+
+```
+npm install
+```
+
+To run a server to see the project working, run:
+
+```
+npm start
+```
--- jquery/ajax.js
+++ jquery/ajax.js
@@ -9,67 +9,67 @@
     // https://api.jquery.com/category/data/
     //
     // Each of these problems builds on the prior one.
-    // It is assumed that you will only run one function at 
-    // a time, so the first step for doing the subsequent 
+    // It is assumed that you will only run one function at
+    // a time, so the first step for doing the subsequent
     // problem will be copy/pasting your solution to the last one
     // and then modifying it.
-    
+
     /**
      * Problem 0: In the .ajax-form div, there are some controls.
-     * When the GO button is clicked, make a request to 
-     *      /ajax/go
+     * When the GO button is clicked, make a request to
+     *      /ajax-debug/go
      * and put the response in the .data-response div.
      */
     function fillResponse() {
-        
+
     }
-    
+
     /**
      * Problem 1: Instead of hardcoding the path to be /ajax/go,
-     * use the value in the "Request path" field instead. The 
+     * use the value in the "Request path" field instead. The
      * server will give a debugging response to any request path
-     * starting with /ajax, so you may wish to try out some of 
-     * those requests. 
+     * starting with /ajax, so you may wish to try out some of
+     * those requests.
      */
     function makeRequestToPath() {
-        
+
     }
-    
+
     /**
      * Problem 2: Allow the user to write json in the "Request data"
      * field. Send that json to the server as request data in your
      * ajax call. Display the result in the .data-response div.
      */
     function makeRequestWithData() {
-        
+
     }
-    
+
     /**
      * Problem 3: When the "GO" button is hit, change the .data-response
      * div to say "Loading..." until the response comes back.
      */
     function addLoadingIndicator() {
-        
+
     }
-    
+
     /**
      * Problem 4: In addition to the response body, display response
      * headers in the .data-response-headers table. Each header should
      * have one row in the table.
-     * 
+     *
      * Hint: the return value of the jQuery AJAX call may have
-     * some useful properties and methods on it.    
+     * some useful properties and methods on it.
      */
     function displayResponseHeaders() {
-        
+
     }
-    
+
     /**
      * Problem 5: Use the "custom request header key" and "custom request header value"
      * methods to allow the user to specify their own request header to send with
      * the ajax request.
      */
     function sendCustomHeaders() {
-        
+
     }
 })();
